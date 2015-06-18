efd30031468787ea5f0d0e9efa0be3d88db86888
Mohan <mohan@thoughtnotch.com>
2015-06-18 22:12:52 +0530

    Grunt file sass include file corrected

--- Gruntfile.js
+++ Gruntfile.js
@@ -212,7 +212,7 @@ module.exports = function (grunt) {
         imagesDir: '<%= yeoman.app %>/images',
         javascriptsDir: '<%= yeoman.app %>/scripts',
         fontsDir: '<%= yeoman.app %>/styles/fonts',
-        importPath: './bower_components',
+        importPath: ['./bower_components','./app/sass'],
         httpImagesPath: '/images',
         httpGeneratedImagesPath: '/images/generated',
         httpFontsPath: '/styles/fonts',
