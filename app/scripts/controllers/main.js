bdf808a7a69aea3e32b917afb19eb10eb316bd85
Mohan <mohan@thoughtnotch.com>
2015-06-18 22:05:42 +0530

    initial files

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name monyenApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the monyenApp
+ */
+angular.module('monyenApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
